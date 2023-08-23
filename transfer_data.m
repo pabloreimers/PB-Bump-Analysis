a06ef4f00853227f39d968384a9e05c2ddf1858c
Pablo Reimers <preimers24@gmail.com>
2023-08-23 16:16:28 -0400

    script updates, includes smoothing a nd removes 3d bump calc

--- transfer_data.m
+++ transfer_data.m
@@ -1,27 +1,70 @@
-local_dir            = 'C:\Users\ReimersPabloAlejandr\Documents\Data\2p data\'; %define the local directory to clear up
-remote_dir           = '\\files.med.harvard.edu\Neurobio\Wilson Lab\pablo\lpsp_kir\'; %define the remote directory to make sure things are backed up to
+local_dir            = 'C:\Users\ReimersPabloAlejandr\Documents\Data\2p data\current\'; %define the local directory to clear up
+remote_dir           = '\\files.med.harvard.edu\Neurobio\wilsonlab\pablo\gain_change\'; %define the remote directory to make sure things are backed up to
 
 %% check whether files exist in the remote dir
 date_folders        = cellstr(ls(local_dir)); %store all the subfolders, which include the dates
 date_folders(1:2) = []; %clean up this variable
 
 for i = date_folders'
-    local_trials = cellstr(ls([local_dir,i{1}]));
-    local_trials(1:2,:) = [];
-    remote_trials= cellstr(ls([remote_dir,i{1}]));
-    remote_trials(1:2,:) = [];
+    tmp_local = [local_dir,'\',i{1}];
+    tmp_remote= [remote_dir,'\',i{1}];
     
+    local_flies = cellstr(ls(tmp_local));
+    local_flies(1:2,:) = [];
+    remote_flies= cellstr(ls(tmp_remote));
+    remote_flies(1:2,:) = [];
+
+    for f = local_flies'
+    
+    tmp_local = [local_dir,'\',i{1},'\',f{1}];
+    tmp_remote= [remote_dir,'\',i{1},'\',f{1}];
+
+    local_trials = cellstr(ls(tmp_local));
+    local_trials(1:2,:) = [];
+    remote_trials= cellstr(ls(tmp_remote));
+    remote_trials(1:2,:) = [];
+
     for j = local_trials'
-        local_contents = cellstr(ls([local_dir,i{1},'\',j{1}]));
-        remote_contents = cellstr(ls([remote_dir,i{1},'\',j{1}]));
+        tmp_local = [local_dir,'\',i{1},'\',f{1},'\',j{1}];
+        tmp_remote= [remote_dir,'\',i{1},'\',f{1},'\',j{1}];
+
+        local_contents = cellstr(ls(tmp_local));
+        local_contents(1:2) = [];
+        remote_contents = cellstr(ls(tmp_remote));
+        remote_contents(1:2) = [];
         
+        fprintf(['current folder: ',j{1},'\n'])
+
+        outer_local = tmp_local;
+        outer_remote = tmp_remote;
         for k = local_contents'
-            if isfolder([local_dir,i{1},'\',j{1},'\',k{1}])
-                a=1
-            elseif ~exists([remote_dir,i,'\',j,'\',k])
-                    copyfile [local_dir,i,'\',j] [remote_dir,i,'\',j]
+            tmp_local = [outer_local,'\',k{1}];
+            tmp_remote= [outer_remote,'\',k{1}];            
+
+
+            if isfolder(tmp_local) %if the current content is a folder, check whether it exists in the remote. if yes, delete it, if no, copy and then delete 
+                if isfolder(tmp_remote)
+                    if ~contains(k{1},'registration')
+                        rmdir(tmp_local,'s')
+                    end
+                else
+                    mkdir(tmp_remote)
+                    copyfile(tmp_local,tmp_remote)
+                end
+                
+            elseif ~isfile(tmp_remote) %if it's a not a folder, check whether it exists in remote dir. if no, copy it and delete it locally
+                copyfile(tmp_local, [remote_dir,i{1},'\',f{1},'\',j{1}])
+                if ~contains(k{1},'imagingData') && ~contains(k{1},'ficTracData_DAQ') && ~contains(k{1},'mask')
+                    delete(tmp_local)
+                end
+            else %if it does exist in the remote dir, delete it locally unless it's the imaging file
+                if ~contains(k{1},'imagingData') && ~contains(k{1},'ficTracData_DAQ') && ~contains(k{1},'mask')
+                    delete(tmp_local)
+                end
             end
         end
     end
+    end
+    fprintf(['current folder: ',j{1},'\n'])
 end
 
